0b8f9052746fa820e5e64833019fee2758414e34
Zach Cervi <zachcervi@gmail.com>
2022-02-03 10:24:24 -0700

    Sort by Select

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,7 +1,8 @@
-//filters reducer
+// Filters Reducer
+
 const filtersReducerDefaultState = {
   text: "",
-  sortBy: "date", //date or amount
+  sortBy: "date",
   startDate: undefined,
   endDate: undefined,
 };
@@ -9,16 +10,30 @@ const filtersReducerDefaultState = {
 export default (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
     case "SET_TEXT_FILTER":
-      return { ...state, text: action.text };
+      return {
+        ...state,
+        text: action.text,
+      };
     case "SORT_BY_AMOUNT":
-      return { ...state, sortBy: "amount" };
+      return {
+        ...state,
+        sortBy: "amount",
+      };
     case "SORT_BY_DATE":
-      return { ...state, sortBy: "date" };
+      return {
+        ...state,
+        sortBy: "date",
+      };
     case "SET_START_DATE":
-      return { ...state, startDate: action.startDate };
+      return {
+        ...state,
+        startDate: action.startDate,
+      };
     case "SET_END_DATE":
-      return { ...state, endDate: action.endDate };
-
+      return {
+        ...state,
+        endDate: action.endDate,
+      };
     default:
       return state;
   }
