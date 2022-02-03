541a73f547b2b02143b44bfa10229c59cb2603d8
Zach Cervi <zachcervi@gmail.com>
2022-02-02 18:41:52 -0700

    Setup Redux, moved from playground

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "../src/styles/styles.scss";
+
+const store = configureStore();
+store.dispatch(
+  addExpense({ description: "Water Bill", amount: 100, createdAt: -21000 })
+);
+store.dispatch(
+  addExpense({ description: "Electric Bill", amount: 200, createdAt: 12500 })
+);
+store.dispatch(setTextFilter("elec"));
+const state = store.getState();
+const visibleExpense = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpense);
 
 ReactDOM.render(<AppRouter />, document.getElementById("app"));
---
+++ src/playground/destructuring.js
@@ -0,0 +1,44 @@
+console.log("destructuring");
+
+// const person = {
+//   name: "Zach",
+//   age: 28,
+//   location: {
+//     city: "Thornton",
+//     temp: 41,
+//   },
+// };
+// // const name = person.name
+// // const age = person.age
+
+// const { name: firstName = "Anonymous", age } = person;
+
+// console.log(`${firstName} is ${age}.`);
+// const { city, temp: temperature } = person.location;
+// if (city && temperature) {
+//   console.log(`It is ${temperature} in ${city}`);
+// }
+
+// const book = {
+//     title: 'Harry Potter 2',
+//     author: 'JK Rowling',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// }
+// const {name: publisherName = "Self-Published" } = book.publisher
+// console.log(publisherName);
+
+// const address = ['5054 E 125th Ave', 'Thornton', 'CO', '80241'];
+
+// const [,city , state="Colorado"] = address;
+
+// console.log(`You are in ${city} ${state}.`)
+
+
+
+// const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75' ]
+
+// const [drink, , medPrice] = item;
+
+// console.log(`A medium ${drink} costs ${medPrice}`);
