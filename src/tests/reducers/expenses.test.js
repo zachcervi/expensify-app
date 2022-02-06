79a293bb1ca3e39d0fc7ee194932c6668437880f
Zach Cervi <zachcervi@gmail.com>
2022-02-06 12:49:23 -0700

    moved test data into common fixture file and completed expenses reducer test

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,70 @@
+import moment from "moment";
+import expensesReducer from "../../reducers/expenses";
+import expenses from "../fixtures/expenses";
+
+test("should set default state", () => {
+  const state = expensesReducer(undefined, { type: "@@INIT" });
+  expect(state).toEqual([]);
+});
+
+test("should remove expense by id", () => {
+  const action = {
+    type: "REMOVE_EXPENSE",
+    id: expenses[1].id,
+  };
+
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([expenses[0], expenses[2]]);
+});
+
+test("should not remove expenses if id is not found", () => {
+  const action = {
+    type: "REMOVE_EXPENSE",
+    id: "-1",
+  };
+
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual(expenses);
+});
+
+test("should add a new expense to existing expenses ", () => {
+  const expense = {
+    description: "Lego",
+    note: "",
+    amount: 19900,
+    createdAt: 20000,
+  };
+
+  const action = {
+    type: "ADD_EXPENSE",
+    expense,
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([...expenses, expense]);
+});
+
+test("should edit an expense", () => {
+  const amount = 122000;
+  const action = {
+    type: "EDIT_EXPENSE",
+    id: expenses[1].id,
+    updates: {
+      amount,
+    },
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state[1].amount).toBe(amount);
+});
+
+test("should not edit an expense if expense not found", () => {
+  const amount = 122000;
+  const action = {
+    type: "EDIT_EXPENSE",
+    id: "4564689",
+    updates: {
+      amount,
+    },
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual(expenses);
+});
--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -1,28 +1,6 @@
 import selectExpenses from "../../selectors/expenses";
 import moment from "moment";
-const expenses = [
-  {
-    id: "1",
-    description: "Gum",
-    note: "",
-    amount: 195,
-    createdAt: 0,
-  },
-  {
-    id: "2",
-    description: "Rent",
-    note: "",
-    amount: 109500,
-    createdAt: moment(0).subtract(4, "days").valueOf(),
-  },
-  {
-    id: "3",
-    description: "Credit Card",
-    note: "",
-    amount: 4500,
-    createdAt: moment(0).add(4, "days").valueOf(),
-  },
-];
+import expenses from "../fixtures/expenses";
 
 test("should filter by text value", () => {
   const filters = {
@@ -68,17 +46,17 @@ test("should sort by date", () => {
   };
 
   const result = selectExpenses(expenses, filters);
-  expect(result).toEqual([expenses[2], expenses[0], expenses[1]])
+  expect(result).toEqual([expenses[2], expenses[0], expenses[1]]);
 });
 
 test("should sort by amount", () => {
-    const filters = {
-      text: "",
-      sortBy: "amount",
-      startDate: undefined,
-      endDate: undefined,
-    };
+  const filters = {
+    text: "",
+    sortBy: "amount",
+    startDate: undefined,
+    endDate: undefined,
+  };
 
-    const result = selectExpenses(expenses, filters);
-    expect(result).toEqual([expenses[1], expenses[2], expenses[0]]);
-})
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([expenses[1], expenses[2], expenses[0]]);
+});
